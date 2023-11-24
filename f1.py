33061732931ddcde6a74ac63c1c7597c1cfd911e
Akshay Singh Chauhan <142593499+akshaytheGodxo@users.noreply.github.com>
2023-11-24 10:40:38 +0530

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,10 @@
+def area(length , breadth):
+    print (length * breadth)
+
+def parameter(length , breadth):
+    print (2 * (length + breadth))
+
+x = float(input("Enter the length: "))
+y = float(input("Enter the breadth : "))
+area(x,y)
+parameter(x,y)
